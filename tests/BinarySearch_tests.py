b1d4e7b752fce8ee70a0a1c78ffac0a9ed7d8dc1
Issa Beekun <goodflyerfye@gmail.com>
2014-12-14 22:44:50 -0800

    Updated unit tests to separate each test case into its own test.

--- tests/BinarySearch_tests.py
+++ tests/BinarySearch_tests.py
@@ -2,30 +2,41 @@ from nose.tools import assert_equal
 from Tabl_homework.BinarySearch import *
 
 def test_list_sorting():
+    #test to make sure the quick-sort algorithm works properly
     input_list = [5, 6, 12, 2, 1, 11, 4, 3, 7]
     sort_list(input_list)
     assert_equal(input_list, [1, 2, 3, 4, 5, 6, 7, 11, 12])
     
-def test_binary_search():
-    input_list = [1, 2, 3, 4, 5, 6, 7, 11, 12]
+def test_binary_search_num_in_middle():
     #test when element is present 
-    #in list
+    input_list = [1, 2, 3, 4, 5, 6, 7, 11, 12]
     assert_equal(binary_search(input_list, 6), 5)
-    #at either end of list
-    assert_equal(binary_search(input_list, 1), 0)
-    assert_equal(binary_search(input_list, 12), 8)    
     
+def test_binary_search_num_at_start():
+    #test when element is present at start of list
+    input_list = [1, 2, 3, 4, 5, 6, 7, 11, 12]
+    assert_equal(binary_search(input_list, 1), 0)
+    
+
+def test_binary_search_num_at_end():
+    #test when element is present at end of list
+    input_list = [1, 2, 3, 4, 5, 6, 7, 11, 12]
+    assert_equal(binary_search(input_list, 12), 8)  
+    
+
+def test_binary_search_not_in_list():
     #test when element is not present
+    input_list = [1, 2, 3, 4, 5, 6, 7, 11, 12]
     assert_equal(binary_search(input_list, 8), -1)
     
 def test_quick_sort():
-    #ensures the index of the initial pivot is returned to quick_sort correctly 
+    #test to ensure the index of the initial pivot is returned to quick_sort correctly 
     input_list = [5, 6, 12, 2, 1, 11, 4, 3, 7]
-
     assert_equal(quick_sort(input_list, 0, len(input_list)-1), 6)
     
 def test_partition():
-    #ensures the index of the initial pivot is unchanged 
+    #test to ensures the index of the initial pivot is selected properly
     input_list = [5, 6, 12, 2, 1, 11, 4, 3, 7]
-
     assert_equal(partition(input_list, 0, len(input_list)-1), 6)
+    
+    
--- tests/WordReversal_tests.py
+++ tests/WordReversal_tests.py
@@ -1,36 +1,52 @@
 from nose.tools import assert_equal
 from Tabl_homework.WordReversal import *
 
-def test_is_letter():
+def test_is_letter_a_letter():
     #Test character
     assert_equal(is_letter('t'), True)
     
+def test_is_delimeter_a_letter():
     #Test delimiter
     assert_equal(is_letter('?'), False)
     
+def test_is_number_a_letter():
     #Test number
     assert_equal(is_letter('3'), False)
     
+def test_is_empty_a_letter():
     #Test empty string
     assert_equal(is_letter(''), False)
     
+def test_is_word_a_letter():
     #Test word, not a single character
     assert_equal(is_letter('word'), False)
-
-
-def test_word_reverse():
+    
+def test_word_reverse_sentence():
     #Tests standard sentence
     assert_equal(word_reverse('I am   a sentence!!!'), 'sentence a   am I!!!')
     
+def test_word_reverse_empty_string():
     #Tests empty string
     assert_equal(word_reverse(''), '')
     
+def test_word_reverse_one_word():
     #Tests sentence with only one word
     assert_equal(word_reverse('word'), 'word')
     
+def test_word_reverse_delimiter_start():
     #Tests sentence starting and ending with a delimiter
     assert_equal(word_reverse('!Ay caramba!'), '!caramba Ay!')
     
+def test_word_reverse_word_end():
     #Tests sentence starting and ending with a word/character
     assert_equal(word_reverse('and/or'), 'or/and')
+    
+def test_word_reverse_numbers_and_words():
+    #Tests sentence starting and ending with a word/character
+    assert_equal(word_reverse('R2D2 and C3PO at your service!'), 'service2your2 at PO3C and D R!')
+    
+    
+    
+    
+    
 
